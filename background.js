13b5e7163f8aa23f6189acec0a2b85e7d7375287
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-02 17:31:15 +0200

    chore: delete duplicates

--- background.js
+++
@@ -1,8 +0,0 @@
-const tabs = typeof browser !== "undefined" ? browser.tabs : chrome.tabs;
-const runtime = typeof browser !== "undefined" ? browser.runtime : chrome.runtime;
-
-runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === "open_tab") {
-        tabs.create({ url: message.url, active: false });
-    }
-});
